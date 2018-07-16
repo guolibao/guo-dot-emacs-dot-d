6f8049311e698458a7e60f1e074757a1ae72730f
Guo LiBao <guolb8245@jkts.jvckenwood.com>
2018-07-16 09:39:45 +0800

    edit files

--- config.el
+++ config.el
@@ -33,6 +33,9 @@
                         (define-key company-active-map (kbd "C-p") #'company-select-previous)
                         (define-key company-active-map (kbd "SPC") #'company-abort))
 
+(use-package ggtags
+  :ensure t)
+
 (add-hook 'c++-mode 'yas-minor-mode)
 (add-hook 'c-mode-hook 'yas-minor-mode)
 
@@ -48,12 +51,20 @@
   (add-hook 'c-mode-hook 'company-mode))
 
 (use-package company-c-headers
-  :ensure t
-  :config
-  ;; (setq company-backends '((company-c-headers
-  ;;                           company-dabbrev-code
-  ;;                           company-irony)))
-  )
+  :ensure t)
+
+(defun c-mode-company-init ()
+  (setq company-backends '((company-c-headers
+                            company-dabbrev-code
+                            company-irony))))
+
+(use-package company-irony
+  :ensure t)
+
+(with-eval-after-load 'company
+  (add-hook 'c++-mode-hook 'c-mode-company-init)
+  (add-hook 'c-mode-hook 'c-mode-company-init))
+
 
 (use-package irony
   :ensure t
@@ -61,6 +72,10 @@
   (add-hook 'c++-mode-hook 'irony-mode)
   (add-hook 'c-mode-hook 'irony-mode)
   (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options))
+
+(with-eval-after-load 'ggtags
+  (add-hook 'c++-mode-hook 'ggtags-mode)
+  (add-hook 'c-mode-hook 'ggtags-mode))
 
 (add-hook 'python-mode-hook 'yas-minor-mode)
 
@@ -115,3 +130,34 @@
   :config
   (require 'company)
   (add-hook 'shell-mode-hook 'shell-mode-company-init))
+
+(defadvice align-regexp (around align-regexp-with-spaces activate)
+  (let ((indent-tabs-mode nil))
+    ad-do-it))
+
+;; disable auto-save and auto-backup
+(setq auto-save-default nil)
+(setq make-backup-files nil)
+;; don't use lockfiles to avoid editng collisions
+(setq create-lockfiles nil)
+
+;; store all backup and autosave files in the tmp dir
+;; (setq backup-directory-alist
+;;       `((".*" . ,temporary-file-directory)))
+;; (setq auto-save-file-name-transforms
+;;       `((".*" ,temporary-file-directory t)))
+
+;; highlight-symbol key binding
+(use-package highlight-symbol
+  :ensure t
+  :config
+  (global-set-key [(shift f8)] 'highlight-symbol)
+  (global-set-key [f8] 'highlight-symbol-next)
+  (global-set-key [(control f8)] 'highlight-symbol-prev)
+  (global-set-key [(meta f8)] 'highlight-symbol-query-replace))
+
+(use-package google-translate
+  :ensure t
+  :config
+  (setq google-translate-translation-directions-alist
+        '(("ja" . "en") ("en" . "de") ("de" . "fr") ("fr" . "de"))))
