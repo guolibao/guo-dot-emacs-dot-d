f831bb4c2550f6ec4347ea78b99b1c5f71b68195
GuoLiBao <guolb8245@jkts.jvckenwood.com>
2018-07-17 16:27:45 +0800

    edit something

--- init.el
+++ init.el
@@ -33,7 +33,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (rainbow-mode smex ido-vertical-mode ace-window use-package))))
+    (htmlize rainbow-mode smex ido-vertical-mode ace-window use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
