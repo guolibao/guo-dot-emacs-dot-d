d173601ff456147066491c0a8d26352249de5498
GuoLiBao <guolb8245@jkts.jvckenwood.com>
2018-08-10 09:23:34 +0800

    add some more dir-locals element

--- local-dir-clang-complete-example/.dir-locals.el
+++ local-dir-clang-complete-example/.dir-locals.el
@@ -6,4 +6,11 @@
 	(setq company-c-headers-path-user
 	      (delete-dups
 	       (append company-c-headers-path-user
-		       (list "aa" "bb" "F:/Qt/Qt5.8.0/5.8/mingw53_32/include")))))))
+		       (list "aa" "bb" "F:/Qt/Qt5.8.0/5.8/mingw53_32/include"))))))
+ (c++-mode
+  (eval progn
+	(setq company-clang-arguments
+	      (delete-dups
+	       (append company-clang-arguments
+		       (list "-std=c++14" "-I/usr/include/SDL2")))))))
+
